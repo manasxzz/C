819f27a88ce5f052f72b07d1c93a7ffa48317444
Manas <118667123+manasxzz@users.noreply.github.com>
2022-11-20 21:46:23 +0530

    Update addsubdivmul.c

--- addsubdivmul.c
+++ addsubdivmul.c
@@ -7,6 +7,6 @@ void main()
   scanf("%d",&a);
   printf("\nEnter Second Number: ");
   scanf("%d",&b);
-  printf("\nThe sum of the given numbers=%d\nThe ",a+b);
+  printf("\nThe sum of the given numbers=%d\nThe difference of the given numbers=%d\The product of the given numbers=%d\n%d divided by%d=%d ",a+b,a-b,a*b,a,b,a/b);
   getch();
 }
