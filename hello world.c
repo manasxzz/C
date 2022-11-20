adc0dccdeaeaac3a2ffed149064984a7d2748e97
Manas <118667123+manasxzz@users.noreply.github.com>
2022-11-20 18:50:17 +0530

    Update hello world.c

--- hello world.c
+++ hello world.c
@@ -3,7 +3,5 @@
 void main()
 {
     printf("Hello World!\n");
-    printf("This is the change I made in github!\n");
-    printf("This is the second change I made in the github!\n");
     getch();
 }
