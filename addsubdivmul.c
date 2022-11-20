eeed66455f2e828baeac4fe19b1115b69f555778
Manas <118667123+manasxzz@users.noreply.github.com>
2022-11-20 21:43:28 +0530

    Create addsubdivmul.c

---
+++ addsubdivmul.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+  int a,b;
+  printf("Enter First Number: ");
+  scanf("%d",&a);
+  printf("\nEnter Second Number: ");
+  scanf("%d",&b);
+  printf("\nThe sum of the given numbers=%d\nThe ",a+b);
+  getch();
+}
