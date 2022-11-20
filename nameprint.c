6a4fce4ec84024ffd688f3c0a4d6892f630acf31
Manas <manasmishramm31@gmail.com>
2022-11-20 23:36:05 +0530

    add

--- nameprint.c
+++ nameprint.c
@@ -4,12 +4,12 @@ void main()
 {
  printf("MANAS                              MANASMANAS             MANASMANAS           MANAS                  MANAS             MANASMANAS           MANASMANASMANASMANASMANAS\n");
  printf("MANASMANAS                         MANASMANAS          MANASMANASMANAS         MANASMANAS             MANAS          MANASMANASMANAS         MANASMANASMANASMANASMANAS\n");
- printf("MANASMANAS                         MANASMANAS        MANAS        MANAS        MANAS MANAS            MANAS        MANAS        MANAS        MANAS\n");
- printf("MANASMANASMANAS               MANASMANASMANAS       MANAS          MANAS       MANAS  MANAS           MANAS       MANAS          MANAS       MANAS\n");
- printf("MANASMANASMANAS               MANASMANASMANAS      MANAS            MANAS      MANAS   MANAS          MANAS      MANAS            MANAS      MANAS\n");
- printf("MANAS     MANASMANAS     MANASMANAS     MANAS     MANAS               MANAS    MANAS    MANAS         MANAS     MANAS               MANAS    MANAS\n");
- printf("MANAS     MANASMANAS     MANASMANAS     MANAS    MANAS                 MANAS   MANAS     MANAS        MANAS    MANAS                 MANAS   MANAS\n");
- printf("MANAS          MANASMANASMANAS          MANAS   MANAS                   MANAS  MANAS      MANAS       MANAS   MANAS                   MANAS  MANAS\n");
+ printf("MANASMANAS                         MANASMANAS        MANAS        MANAS        MANAS MANAS            MANAS        MANAS        MANAS        MANAS                    \n");
+ printf("MANASMANASMANAS               MANASMANASMANAS       MANAS          MANAS       MANAS  MANAS           MANAS       MANAS          MANAS       MANAS                    \n");
+ printf("MANASMANASMANAS               MANASMANASMANAS      MANAS            MANAS      MANAS   MANAS          MANAS      MANAS            MANAS      MANAS                    \n");
+ printf("MANAS     MANASMANAS     MANASMANAS     MANAS     MANAS               MANAS    MANAS    MANAS         MANAS     MANAS               MANAS    MANAS                    \n");
+ printf("MANAS     MANASMANAS     MANASMANAS     MANAS    MANAS                 MANAS   MANAS     MANAS        MANAS    MANAS                 MANAS   MANAS                    \n");
+ printf("MANAS          MANASMANASMANAS          MANAS   MANAS                   MANAS  MANAS      MANAS       MANAS   MANAS                   MANAS  MANAS                    \n");
  printf("MANAS               MANAS               MANAS  MANASMANASMANASMANASMANASMANAS  MANAS       MANAS      MANAS  MANASMANASMANASMANASMANASMANAS  MANASMANASMANASMANASMANAS\n");
  printf("MANAS                                   MANAS  MANASMANASMANASMANASMANASMANAS  MANAS        MANAS     MANAS  MANASMANASMANASMANASMANASMANAS  MANASMANASMANASMANASMANAS\n");
  printf("MANAS                                   MANAS  MANAS                    MANAS  MANAS         MANAS    MANAS  MANAS                    MANAS  MANASMANASMANASMANASMANAS\n");
