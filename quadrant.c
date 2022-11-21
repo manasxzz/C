ac4e4c93b18012ca68ff791ac06b5f53cb14817b
Manas <118667123+manasxzz@users.noreply.github.com>
2022-11-21 14:05:01 +0530

    wrote

---
+++ quadrant.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int abscissa, ordinate;
+    printf("Enter the Abscissa: ");
+    scanf("%d",&abscissa);
+    printf("\nEnter the Ordinate: ");
+    scanf("%d",&Ordinate);
+    if(abscissa>0 && ordinate>0)
+    printf("\nThe Given point lies in the Ist Quadrant.");
+    else if(abscissa<0 && ordinate>0)
+    printf("\nThe Given Point lies in the IInd Quadrant.");
+    else if(abscissa<0 && ordinate<0)
+    printf("\nThe Given point lies in the IIIrd Quadrant.");
+    else
+    printf("\nThe Given point lies in the IVth Quadrant.")
+}
