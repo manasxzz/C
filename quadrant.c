fab965bf77334e29652ccebb9b9cfc1ba3aa95ab
Manas <manasmishramm31@gmail.com>
2022-11-25 00:52:13 +0530

    defined

--- quadrant.c
+++ quadrant.c
@@ -8,12 +8,20 @@ void main()
     printf("\nEnter the Ordinate: ");
     scanf("%d", &ordinate);
     if (abscissa > 0 && ordinate > 0)
+    {
         printf("\nThe Given point lies in the Ist Quadrant.");
+    }
     else if (abscissa < 0 && ordinate > 0)
+    {
         printf("\nThe Given Point lies in the IInd Quadrant.");
+    }
     else if (abscissa < 0 && ordinate < 0)
+    {
         printf("\nThe Given point lies in the IIIrd Quadrant.");
+    }
     else
-        printf("\nThe Given point lies in the IVth Quadrant.")
-            getch();
+    {
+        printf("\nThe Given point lies in the IVth Quadrant.");
+    }
+    getch();
 }
