15cdb0379abf86e0d9f36e2249dedd689ea403b9
Manas <manasmishramm31@gmail.com>
2022-11-24 21:41:18 +0530

    c

--- calculatorswitch.c
+++ calculatorswitch.c
@@ -2,65 +2,64 @@
 #include <conio.h>
 #include <stdlib.h>
 
-int add(int a, int b)
+long long int add(long long int a, long long int b)
 {
-    return a+b;
+    return a + b;
 }
-int sub(int a,int b)
+long long int sub(long long int a, long long int b)
 {
-    return a-b;
+    return a - b;
 }
-int mul(int a,int b)
+long long int mul(long long int a, long long int b)
 {
-    return a*b;
+    return a * b;
 }
-int divide(int a,int b)
+long long int divide(long long int a, long long int b)
 {
-    return a/b;
+    return a / b;
 }
 void main()
 {
-    int x,y,choice,i=1;
-    while(i<4)
+    long long int x, y, choice, i = 1;
+    while (1)
     {
         system("cls");
         printf("**************Calculator*************");
         printf("\n\t\tMENU");
         printf("\n1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\nENTER CHOICE...");
-        scanf("%d",&choice);
+        scanf("%lld", &choice);
+        system("cls");
         printf("\n\n*********INPUT************\nEnter First Number: ");
-        scanf("%d",&x);
+        scanf("%lld", &x);
         printf("\nEnter Second Number: ");
-        scanf("%d",&y);
-        
-        switch(choice)
+        scanf("%lld", &y);
+        system("cls");
+        switch (choice)
         {
-            case 1:
-            printf("********ADDITION********\n\n%d+%d=%d",x,y,add(x,y));
-            getch();
-            break;
-            
-            case 2:
-            printf("********SUBTRACTION********\n\n%d-%d=%d",x,y,sub(x,y));
+        case 1:
+            printf("********ADDITION********\n\n%lld+%lld=%lld", x, y, add(x, y));
             getch();
             break;
 
-            case 3:
-            printf("********MULTIPLICATION********\n\n%dx%d=%d",x,y,mul(x,y));
+        case 2:
+            printf("********SUBTRACTION********\n\n%lld-%lld=%lld", x, y, sub(x, y));
             getch();
             break;
 
-            case 4:
-            printf("********DIVISION********\n\n%d/%d=%d",x,y,divide(x,y));
+        case 3:
+            printf("********MULTIPLICATION********\n\n%lldx%lld=%lld", x, y, mul(x, y));
             getch();
             break;
 
-            default:
+        case 4:
+            printf("********DIVISION********\n\n%lld/%lld=%lld", x, y, divide(x, y));
+            getch();
+            break;
+
+        default:
             printf("********WRONG INPUT********");
             getch();
             break;
-
         }
-        i++;
     }
 }
